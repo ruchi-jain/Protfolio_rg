3eb7857d0b2643a32c25913471f457cff187fa34
Ruchi Goel <ruchi.goel@outlook.com>
2016-05-22 00:00:10 -0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+   $(document).ready(function(){
+	   $(window).bind('scroll', function() {
+	   var navHeight = $( window ).height() - 50;
+			 if ($(window).scrollTop() > navHeight) {
+				 $('nav').addClass('fixed');
+			 }
+			 else {
+				 $('nav').removeClass('fixed');
+			 }
+		});
+	});
